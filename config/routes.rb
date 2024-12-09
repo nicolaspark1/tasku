6571d835064e5dce936e8c5525626c44826a8f8e
nicolaspark1 <141599069+nicolaspark1@users.noreply.github.com>
2024-12-09 22:27:57 +0000

    new home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "tasks", :action => "index" })
+  devise_for :users
+
+  root to: "tasks#index"
 
   # Routes for the Review resource:
 
